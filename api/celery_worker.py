4673b4c5f9d70c39cd7b524ab7c1f6e55d8a43b4
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2024-04-02 12:18:35 +0200

    celery rdy

--- api/celery_worker.py
+++ api/celery_worker.py
@@ -1,8 +1,10 @@
 from celery import Celery
 
+REDIS_PASSWORD= os.getenv('REDIS_PASSWORD')
+
 celery_app = Celery(
     "worker",
-    broker="redis://demo_redis:6379/0",
+    broker=f"redis://{REDIS_PASSWORD}@demo_redis:6379/0",
     include=["tasks"]  # Assurez-vous de mettre à jour le chemin vers vos tâches Celery
 )
 
