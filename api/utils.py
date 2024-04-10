0b9773f97cba64be5b4adab207223c09d6230130
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2024-04-10 11:31:35 +0200

    ftp logs

--- api/utils.py
+++ api/utils.py
@@ -8,27 +8,21 @@ from tempfile import NamedTemporaryFile
 import subprocess
 import requests
 
+import logging
+
+# Configuration du logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def log_to_ftp(ftp_host: str, ftp_username: str, ftp_password: str, log_message: str, log_folder: str = "logs"):
     """
     Enregistre un message de log dans un dossier spécifié sur un serveur FTP.
-
-    Args:
-    - ftp_host (str): L'hôte du serveur FTP.
-    - ftp_username (str): Le nom d'utilisateur pour se connecter au serveur FTP.
-    - ftp_password (str): Le mot de passe pour se connecter au serveur FTP.
-    - log_message (str): Le message à enregistrer dans le fichier de log.
-    - log_folder (str): Le dossier sur le serveur FTP où le fichier de log sera enregistré.
     """
-
-    # Crée un nom de fichier basé sur la date et l'heure actuelle
     tz = pytz.timezone('Europe/Paris')
-
-    now =datetime.now(tz)
-
+    now = datetime.now(tz)
     log_filename = f"log_{now.strftime('%Y%m%d_%H%M%S')}.txt"
     log_file_path = os.path.join(log_folder, log_filename).replace('\\', '/')
     
-    print(f"Tentative de log FTP dans : {log_file_path}")  # Débogage
+    logging.info(f"Tentative de log FTP dans : {log_file_path}")
     
     with NamedTemporaryFile("w", delete=False) as temp_log_file:
         temp_log_file.write(log_message)
@@ -36,20 +30,23 @@ def log_to_ftp(ftp_host: str, ftp_username: str, ftp_password: str, log_message:
 
     try:
         with FTP(ftp_host, ftp_username, ftp_password) as ftp:
+            logging.info(f"Connexion établie avec {ftp_host}")
             ftp.cwd('/')  # Assurez-vous d'être à la racine
-            if log_folder != '/':  # Vérifie si le dossier de logs n'est pas la racine
+            if log_folder != '/':
                 ensure_ftp_path(ftp, log_folder)
             with open(temp_log_path, 'rb') as file:
                 ftp.storbinary(f'STOR {log_file_path}', file)
+                logging.info(f"Fichier {log_file_path} téléversé avec succès.")
     except Exception as e:
-        print(f"Erreur lors du téléversement du log sur FTP : {e}")
+        logging.error(f"Erreur lors du téléversement du log sur FTP : {e}")
     finally:
         os.remove(temp_log_path)  # Nettoyage du fichier temporaire
-
-
+        logging.info("Fichier temporaire supprimé.")
 
 def ensure_ftp_path(ftp, path):
-    """Crée récursivement le chemin sur le serveur FTP si nécessaire."""
+    """
+    Crée récursivement le chemin sur le serveur FTP si nécessaire.
+    """
     path = path.lstrip('/')  # Supprime le slash initial pour éviter les chemins absolus
     directories = path.split('/')
     
@@ -58,10 +55,12 @@ def ensure_ftp_path(ftp, path):
         if directory:  # Ignore les chaînes vides
             current_path += "/" + directory
             try:
-                ftp.cwd(current_path)  # Tente de naviguer dans le dossier
+                ftp.cwd(current_path)
+                logging.info(f"Navigué vers {current_path}.")
             except Exception:
                 ftp.mkd(current_path)  # Crée le dossier s'il n'existe pas
                 ftp.cwd(current_path)  # Navigue dans le dossier nouvellement créé
+                logging.info(f"Dossier {current_path} créé et navigation vers ce dossier.")
 
 def download_docx_file(url: str) -> str:
     """Télécharge un fichier DOCX depuis une URL et retourne le chemin du fichier temporaire."""
